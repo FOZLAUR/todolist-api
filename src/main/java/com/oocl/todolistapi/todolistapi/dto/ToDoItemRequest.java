bd3172dde9ff17c7ac319b70db400ac2b4e22344
FOZLAUR <fozla@oocl.com>
2020-10-29 23:24:37 +0800

    [todolist-api][Laurence&Erik] refactor: Added done to ToDoItemRequest

--- src/main/java/com/oocl/todolistapi/todolistapi/dto/ToDoItemRequest.java
+++ src/main/java/com/oocl/todolistapi/todolistapi/dto/ToDoItemRequest.java
@@ -2,9 +2,11 @@ package com.oocl.todolistapi.todolistapi.dto;
 
 public class ToDoItemRequest {
     private String text;
+    private boolean done;
 
     public ToDoItemRequest(String text) {
         this.text = text;
+        this.done = false;
     }
 
     public String getText() {
@@ -13,5 +15,13 @@ public class ToDoItemRequest {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
     }
 }
