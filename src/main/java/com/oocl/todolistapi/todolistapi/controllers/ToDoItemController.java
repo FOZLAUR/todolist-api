05ba0a305494aaa352088fae124af5921ca8e92e
FOZLAUR <fozla@oocl.com>
2020-10-29 23:30:02 +0800

    [todolist-api][Laurence&Erik] refactor: Integrated Response into ToDoItemController

--- src/main/java/com/oocl/todolistapi/todolistapi/controllers/ToDoItemController.java
+++ src/main/java/com/oocl/todolistapi/todolistapi/controllers/ToDoItemController.java
@@ -1,41 +1,49 @@
 package com.oocl.todolistapi.todolistapi.controllers;
 
+import com.oocl.todolistapi.todolistapi.dto.ToDoItemMapper;
+import com.oocl.todolistapi.todolistapi.dto.ToDoItemResponse;
 import com.oocl.todolistapi.todolistapi.entity.ToDoItem;
 import com.oocl.todolistapi.todolistapi.services.ToDoItemService;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController()
 @RequestMapping("/todos")
 public class ToDoItemController {
-    private ToDoItemService toDoItemService;
+    private final ToDoItemService toDoItemService;
+    private final ToDoItemMapper toDoItemMapper;
 
-    public ToDoItemController(ToDoItemService toDoItemService) {
+    public ToDoItemController(ToDoItemService toDoItemService, ToDoItemMapper toDoItemMapper) {
         this.toDoItemService = toDoItemService;
+        this.toDoItemMapper = toDoItemMapper;
     }
 
     //CREATE
     @PostMapping()
-    public ToDoItem createToDoItem(ToDoItem toDoItem){
-        return toDoItemService.createToDoItem(toDoItem);
+    @ResponseStatus(HttpStatus.CREATED)
+    public ToDoItemResponse createToDoItem(@RequestBody ToDoItem newToDoItem){
+        //ToDoItem toDoItem = toDoItemMapper.toEntity(request);
+        return toDoItemMapper.toResponse(toDoItemService.createToDoItem(newToDoItem));
     }
-
     // READ
     @GetMapping()
-    public List<ToDoItem> getAllToDoItems(){
-        return toDoItemService.getAllToDoItems();
+    public List<ToDoItemResponse> getAllToDoItems() {
+        List<ToDoItem> toDoItemList = toDoItemService.getAllToDoItems();
+        return toDoItemList.stream().map(toDoItemMapper::toResponse).collect(Collectors.toList());
     }
 
     // UPDATE
-    @PutMapping("{taskId}")
-    public ToDoItem updateToDoItem(@PathVariable int taskId){
-        return toDoItemService.updateToDoItem(taskId);
+    @PutMapping("/{taskId}")
+    public ToDoItemResponse updateToDoItem(@PathVariable int taskId){
+        ToDoItem updatedToDoItem = toDoItemService.updateToDoItem(taskId);
+        return toDoItemMapper.toResponse(updatedToDoItem);
     }
 
     // DELETE
-    @DeleteMapping("{taskId}")
+    @DeleteMapping("/{taskId}")
     public void deleteToDoItem(@PathVariable int taskId){
         toDoItemService.deleteToDoItem(taskId);
     }
